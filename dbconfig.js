99956796414fe0554ef06e382ca942f232268377
deanovision <rodeanfraser@gmail.com>
2019-10-19 19:25:26 -0400

    reformatted db config

--- dbconfig.js
+++ dbconfig.js
@@ -1,12 +1,6 @@
 const mongoose = require("mongoose");
 
-const username = process.env.MONGO_DB_USERNAME;
-const password = process.env.MONGO_DB_PW;
-
-mongoose.connect(
-  `mongodb+srv://${username}:${password}@timeclock-08k3l.mongodb.net/test?retryWrites=true&w=majority`,
-  { useNewUrlParser: true }
-);
+mongoose.connect(process.env.MONGO_DB_CONNECTION, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
