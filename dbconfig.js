3100de9e9c5fbde5bb7160eee0e583eb84c6bc42
deanovision <rodeanfraser@gmail.com>
2019-10-20 09:41:13 -0400

    updated database connection

--- dbconfig.js
+++ dbconfig.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
+mongoose.connect(
+  process.env.MONGO_DB_CONNECTION,
+  { useNewUrlParser: true },
+  () => console.log("DB Connected")
+);
 
-mongoose.connect(process.env.MONGO_DB_CONNECTION, { useNewUrlParser: true });
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function() {
-  console.log("connected to database");
-});
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", function() {
+//   console.log("connected to database");
+// });
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const server = require("./server");
+const mongoose = require("mongoose");
 require("dotenv").config();
+
 const port = process.env.PORT || 5001;
+
+mongoose.connect(
+  process.env.MONGO_DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("DB Connected")
+);
 
 server.listen(port, () => {
   console.log("<<<SERVER ON 5K>>>");
