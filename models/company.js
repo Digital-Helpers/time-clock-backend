6d7cc92986edb2cbf517a0bf9b4a9c80c811a2cf
deanovision <rodeanfraser@gmail.com>
2019-10-20 09:40:47 -0400

    setup company model

---
+++ models/company.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const companySchema = new Schema({
+  name: String
+});
+
+module.exports = mongoose.model("Company", companySchema);
