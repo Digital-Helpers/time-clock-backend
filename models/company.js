e824f6bcc36a70db601073fa540ab56e0e39eb53
deanovision <rodeanfraser@gmail.com>
2019-10-22 21:13:45 -0400

    updated company schema

--- models/company.js
+++ models/company.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const companySchema = new Schema({
-  name: String
+  adminName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  employees: [{ type: String, name: String }]
 });
 
 module.exports = mongoose.model("Company", companySchema);
