a714fa95bd039eb3bbf161468cf686dba515d16e
deanovision <rodeanfraser@gmail.com>
2019-10-24 19:44:47 -0400

    setup users router

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const server = express();
+const usersRouter = require("./routes/userRoutes");
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
@@ -9,5 +10,7 @@ server.use(helmet());
 server.get("/", (req, res) => {
   res.send("API Connected");
 });
+
+server.use("/users", usersRouter);
 
 module.exports = server;
