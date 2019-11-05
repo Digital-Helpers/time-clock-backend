91824342769f43cd720e671af8b89c772b926be0
Demetrius <Demetriuslewis2012@gmail.com>
2019-11-05 00:22:10 -0500

    Add logic for routes to find all users,find users by id, modify users by id, delete a user by id, improved the create user.

--- User/userController.js
+++ User/userController.js
@@ -1,18 +1,58 @@
-const mongoose = require("mongoose");
 const User = require("./User");
 
-const createUser = (req, res) => {
-    const user = new User(req.body);
-    
-    user.save()
-    .then(user => {
-      res.status(200).json({ message: 'user saved!'});
+const findAllUsers = (req, res) => {
+  User.find()
+    .then(users => {
+      res.status(200).json(users);
     })
     .catch(err => {
       res.status(500).json({ message: err });
     });
 }
 
+const findUser = (req, res) => {
+  User.find({ _id: req.params.id })
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ message: err });
+    });
+}
+
+const createUser = (req, res) => {
+  User.create(req.body)
+  .then(user => {
+    res.status(201).json({ message: 'user saved!' });
+  })
+  .catch(err => {
+    res.status(500).json({ message: err });
+  });
+}
+
+const modifyUser = (req, res) => {
+  User.updateOne({ _id: req.params.id }, req.body)
+  .then(user => {
+    res.status(200).json({ message: 'user updated!' });
+  })
+  .catch(err => {
+    res.status(500).json({ message: err });
+  })
+}
+
+const deleteUser = (req, res) => {
+  User.deleteOne({ _id: req.params.id })
+  .then(user => {
+    res.status(200).json({ message: 'user delete!' });
+  })
+  .catch(err => {
+    res.status(500).json({ message: err });
+  })
+}
+
 module.exports = {
-    createUser,
+  findAllUsers,
+  createUser,
+  modifyUser,
+  deleteUser
 }
