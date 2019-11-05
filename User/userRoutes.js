d7f76ecfece8a1f494a738f99a39424967f87f25
Demetrius <Demetriuslewis2012@gmail.com>
2019-11-05 00:23:08 -0500

    Added all routes for the user object that matches the controller logic.

--- User/userRoutes.js
+++ User/userRoutes.js
@@ -1,19 +1,20 @@
 const express = require("express");
-const User = require("./User");
-const { createUser } = require('./userController');
+const { findAllUsers, createUser, modifyUser, deleteUser } = require('./userController');
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  User.find()
-    .then(users => {
-      res.status(200).json(users);
-    })
-    .catch(err => {
-      res.status(500).json({ message: err });
-    });
-});
+// Finds all users 
+router.get("/", findAllUsers);
 
+// Finds single user by ID
+router.get("/:id", findAllUsers);
+
+// Creates a new user
 router.post('/', createUser);
 
+// modifies an existing user found by ID
+router.put('/:id', modifyUser);
+
+// delete a user by ID
+router.delete('/:id', deleteUser);
+
 module.exports = router;
-// Test
