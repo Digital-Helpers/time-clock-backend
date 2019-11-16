27ccb3aa4797183cebb47a260ef2ffaff2da63c8
Demetrius <Demetriuslewis2012@gmail.com>
2019-11-16 07:54:27 -0500

    created methods handling company

--- Company/companyController.js
+++ Company/companyController.js
@@ -1,16 +1,59 @@
-const company = require('./Company');
+const Company = require('./Company');
 
 const findAllCompanies = (req, res) => {
-    Company.find()
-    .then(companies => {
-    res.status(200).json(companies);
-    })
-    .catch(err => {
+  Company.find()
+  .then(companies => {
+  res.status(200).json(companies);
+  })
+  .catch(err => {
+  res.status(500).json({ message: err });
+  });
+}
+
+const findCompany = (req, res) => {
+  Company.find({ _id: req.params.company_id })
+  .then(company => {
+  res.status(200).json(company);
+  })
+  .catch(err => {
+  res.status(500).json({ message: err });
+  });
+}
+
+const createCompany = (req, res) => {
+  Company.create(req.body)
+  .then(company => {
+    res.status(200).json(company);
+  })
+  .catch(err => {
+    res.status(500).json({message: err});
+  })
+}
+
+const modifyCompany = (req, res) => {
+  Company.updateOne({ _id: req.params.company_id }, req.body)
+  .then(company => {
+    res.status(200).json({ message: 'Company updated!' });
+  })
+  .catch(err => {
     res.status(500).json({ message: err });
-    });
+  })
+}
+
+const deleteCompany = (req, res) => {
+  Company.deleteOne({ _id: req.params.company_id })
+  .then(company => {
+    res.status(200).json({ message: 'Company deleted!' });
+  })
+  .catch(err => {
+    res.status(500).json({ message: err });
+  })
 }
 
 module.exports = {
     findAllCompanies,
+    findCompany,
     createCompany,
+    modifyCompany,
+    deleteCompany
 }
