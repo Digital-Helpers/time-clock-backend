cb753e8caf8f394bd9c12b1d382259477f837390
Demetrius <Demetriuslewis2012@gmail.com>
2019-11-16 07:51:20 -0500

    replced adminName for owner and required user model ref

--- Company/Company.js
+++ Company/Company.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const companySchema = new mongoose.Schema({
-  owner: { type: String, required: true },
+  owner: { type: Schema.Types.ObjectId, ref: "User" },
+  name: { type: String, required: true},
   email: { type: String, required: true, unique: true },
   phone: { type: String, required: true },
   employees: [{ type: Schema.Types.ObjectId, ref: "User" }]
