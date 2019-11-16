15279dce7bd35dbc02b85fd0c163d5d606895b32
Demetrius <Demetriuslewis2012@gmail.com>
2019-11-16 07:52:36 -0500

    Added controllers for method to create, edit, find, findall, and delete companys

--- Company/companyRoutes.js
+++ Company/companyRoutes.js
@@ -1,7 +1,21 @@
 const router = express.Router();
-const { findAllCompanies } = require("./companyController");
+const { findAllCompanies, findCompany, createCompany, modifyCompany, deleteCompany } = require("./companyController");
 
 // GET ALL COMPANIES
-router.get("/", findAllCompanies);
+router.get('/', findAllCompanies);
+
+// FINDS A COMPANY BY COMPANY_ID
+router.get('/:company_id', findCompany);
+
+// CREATE A COMPANY
+router.post('/', createCompany);
+
+// MODIFIES EXISTING COMPANY FOUND BY ID
+router.put('/:company_id', modifyCompany);
+
+// DELETES A COMPANY FOUND BY ID
+router.delete('/:company_id', deleteCompany);
+
+
 
 module.exports = router;
